c21cce2c1fb35e123d9ccee6fceb7caaeb6a3465
Ärtem-Vorobiovzquitquit()'zz <Artem-Vorobiov>
2018-09-25 14:26:51 +0300

    A bit

--- 3_sciPy.py
+++ 3_sciPy.py
@@ -18,3 +18,24 @@ A = np.array([[1,2],[3,4]])
 B = np.array([[1,2],[3,4]])
 
 print('\n Произведение по элементно \n', A*B)
+
+
+#		Linear Equation
+#	Находим корни уравнения
+from scipy.optimize import root
+
+def func(x):
+	return x + 5 * np.cos(x)
+
+sol = root(func, -1)
+print('\n ROOT: \n')
+print(sol.x, sol.success)
+
+#	Системы нелинейных уравнений
+def func2(x):
+	f = [x[0]*np.cos(x[1])-4,\
+		x[0]*x[1]-x[1]-5]
+	return f
+sol = root(func2,[1,1])
+print('\n FUNC2: \n')
+print(sol.x, sol.success)
